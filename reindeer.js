7d19089a77c2300b0e477facbdae684e0d2b0b3e
Alicia Zaludova <aliciazaludova@gmail.com>
2018-01-21 10:06:12 -0600

    first attempt

--- reindeer.js
+++ reindeer.js
@@ -1,9 +1,13 @@
 var colors = ["Blue", "Red", "Orange", "Purple", "Hazel", "Aquamarine", "Periwinkle", "Azure", "Fuchsia", "Chocolate", "Amber", "Amaranth"];
-var reindeer = ["Dasher", "Dancer", "Prancer", "Vixen", "Comet", "Cupid", "Donner", "Blitzen"];
+console.log(colors);
 
+var reindeer = ["Dasher", "Dancer", "Prancer", "Vixen", "Comet", "Cupid", "Donner", "Blitzen"];
+console.log(reindeer);
 
 var hohohoElement = document.getElementById("reindeer");
 
-for (var i = 0, i < colors.length, i++) {
-    var string = 
+for (let i = 0; i < colors.length; i++) {
+    var coloredReindeer = colors.concat("reindeer");
+    console.log(coloredReindeer);
+    // this prints the colors 13 times, reindeer once
 }
