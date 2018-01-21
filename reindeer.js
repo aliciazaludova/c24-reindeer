80a15d2160a77ce757c4d2a7942cf2e13d8ddcf7
Alicia Zaludova <aliciazaludova@gmail.com>
2018-01-21 10:31:39 -0600

    reindeer names generated

--- reindeer.js
+++ reindeer.js
@@ -6,8 +6,17 @@ console.log(reindeer);
 
 var hohohoElement = document.getElementById("reindeer");
 
+var coloredReindeer =[];
+
 for (let i = 0; i < colors.length; i++) {
+    for (let j = 0; j < reindeer.length; j++) {
+        coloredReindeer.push(colors[i] + reindeer[j]);
+    }
+    // *** WHY PUSH?
+    // *** is the var coloredReindeer above [] left empty because ______???
+    /* originally tried just to see what happens:
     var coloredReindeer = colors.concat("reindeer");
-    console.log(coloredReindeer);
-    // this prints the colors 13 times, reindeer once
+    console.log(coloredReindeer); 
+    it printed the colors 13 times, reindeer once. */
+    hohohoElement.innerHTML += coloredReindeer;
 }
